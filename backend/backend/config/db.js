ac50ec84bdc763620c94f733d28ff222103a629f
OrangeSpacer <maxaev.art@yandex.ru>
2022-10-16 11:51:46 +0300

    feat: add config for data base

---
+++ backend/backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+export const connectDB = async () => {
+    try{
+        const connect = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+        console.log(`MongoDB connected: ${connect.connection.host}`.cyan.underline)
+    }
+    catch(e){
+        console.log(e)
+        process.exit(1)
+    }
+}
---
+++ backend/backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import colors from "colors";
+
+// Config
+
+// MiddleWare
+import { connectDB } from "./config/db";
+
+// Routes
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+if(process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+
+app.use(express.json())
+
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, function(err){
+    if(err){
+        console.log("Error in server setup")
+    }
+    console.log("Server listeting on PORT",PORT)
+})
