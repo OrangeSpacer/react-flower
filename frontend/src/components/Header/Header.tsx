eb55005b64b028342ec78ace473de6075837c90a
OrangeSpacer <maxaev.art@yandex.ru>
2022-10-16 19:23:13 +0300

    feat: add header and burger component and adpative for 320px

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,51 @@
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Burger from "../Burger/Burger";
+import styles from "./Header.module.scss";
+import { LinkList } from "./HeaderLinkList";
+
+const Header = () => {
+  const [openContent,setOpenContent] = useState(false)
+
+  const openContentHandle = () => {
+    setOpenContent(!openContent)
+  }
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__content} style={openContent ? {transform:'translateX(0)',transition:'all 0.5s ease'}:{}}>
+        <Link to="/">
+            <img src="img/header/logo.svg" alt="logo" />
+        </Link>
+        <div className={styles.header__list}>
+            <Burger linkList={LinkList}/>
+        </div>
+        <div className={styles.header__info}>
+          <div className={styles.header__info_auth}>
+            <Link to="/auth" className={styles.header__info_link}>
+              <img src="img/header/login.svg" alt="login"/>
+            </Link>
+          </div>
+          <div className={styles.header__info_phone}>
+              <a href="tel:+375291136969" className={styles.header__info_link}>
+                <img src="img/header/phone.svg" alt="phone"/>
+                +375 (29) 113-69-69
+              </a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.header__name}>
+        Lower Flower
+      </div>
+      <div className={styles.header__cart} style={{color:'white'}}>
+        test
+            {/* Компонент Корзины */}
+      </div> 
+      <div className={styles.header__openContent} onClick={openContentHandle}>
+        {openContent ? <img src="/img/header/close.svg" alt="close"/>:<img src="/img/header/burger.svg" alt="burger"/>}
+      </div>
+    </header>
+  )
+}
+
+export default Header
