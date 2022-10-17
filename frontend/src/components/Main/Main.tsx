ccee3720c897bce71970f233675d41b10bc58cd1
OrangeSpacer <maxaev.art@yandex.ru>
2022-10-17 21:57:30 +0300

    feat: add main component and adaptive him for 320px

---
+++ frontend/src/components/Main/Main.tsx
@@ -0,0 +1,28 @@
+import { MainProps } from './Main.props'
+
+import styles from  './Main.module.scss'
+import Button from '../UI/Button/Button'
+
+const Main = ({}:MainProps) => {
+  return (
+    <div className={styles.main}>
+        <img src="img/Main/mainBG.png" alt="background" className={styles.img}/>
+        <div>
+            <h1 className={styles.title}>
+                Lower Flower
+            </h1>
+            <div className={styles.subtitle}>
+                Создаём для тех, кто ценит свежесть и изящество цветка
+            </div>
+            <div className={styles.btnBlock}>
+                <Button onClick={() => console.log("Смотреть каталог")}>
+                    смотреть каталог
+                </Button>
+            </div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ frontend/src/components/UI/Button/Button.tsx
@@ -0,0 +1,13 @@
+import { ButtonProps } from './Button.props'
+
+import styles from './Button.module.scss'
+
+const Button = ({children,...props}:ButtonProps) => {
+  return (
+    <button className={styles.button} {...props}>
+        {children}
+    </button>
+  )
+}
+
+export default Button
